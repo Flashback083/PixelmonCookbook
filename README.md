5bed8959fa69d2b3ac170997ba5d53c40231ae0a
TSEcho <21craftn@gmail.com>
2019-05-16 13:59:24 -0400

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # PixelmonCookbook
 Documented example plugin for Pixelmon Reforged
+
+## Contributing
+
+1. Clone the repository at https://github.com/TSEcho/PixelmonCookbook
+2. Create a branch with a descriptive but succinct name
+3. Make your changes to the code
+4. Add and commit your changes to your branch
+5. Push to your changes to github
+6. Submit a pull request via github
+7. Your pull request will be approved shortly
---
+++ src/main/java/com/pixelmonmod/cookbook/examplemod/ExampleMod.java
@@ -0,0 +1,21 @@
+package com.pixelmonmod.cookbook.examplemod;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = ExampleMod.MOD_ID,
+        name = ExampleMod.MOD_NAME,
+        version = ExampleMod.VERSION)
+
+public class ExampleMod {
+
+    /*
+      This mod showcases many of the various classes, enums, and methods from the Pixelmon Reforged API
+     */
+
+    public static final String MOD_ID = "ExampleMod";
+    public static final String MOD_NAME = "ExampleMod";
+    public static final String VERSION = "1.0.0";
+
+    @Mod.Instance(MOD_ID)
+    public static ExampleMod INSTANCE;
+}
